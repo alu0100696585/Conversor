24b88e2f5bd6626166b9d13568aac82b107d0c13
Juvenal Santiso Hernandez <god.juvenal@gmail.com>
2014-01-29 17:12:32 +0000

    Inicio

---
+++ temperatura.js
@@ -0,0 +1,29 @@
+"use strict"; // Use ECMAScript 5 strict mode in browsers that support it
+function calculate() {
+  var sol;
+  var element= document.getElementById("original");
+  var temp = element.value;
+  var reg = /[+-]?(\d+)(?:\.\d*)?\s*([FfCc])/;
+  
+  var ex = temp.match(reg);
+  
+  if (ex) {
+    var num = ex[0];   // Se podría coger 0 o 1 ya que despues lo convertimos a float.
+                       // Si usamos 0 que es la cadena con la letra inclusive, la quitamos más adelante con la convercion.
+    var type = ex[2];  // La letra del tipo de grado 
+    num = parseFloat(num);
+    if (type == 'c' || type == 'C') {
+      sol = (num * 9/5)+32;
+      sol = sol.toFixed(1) + "F";
+    }
+    else {
+      sol= (num - 32)*5/9;
+      sol= sol.toFixed(1) + "C";
+    }
+    converted.innerHTML = sol;
+  }
+  else {
+    converted.innerHTML = "WRONG! Try something like '-4.2C' instead";
+  }
+}
+  
